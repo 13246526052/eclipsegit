9f808f6c343657c3a74b2cd9fdb8a92f2944cc7f
Administrator <Administrator@Hasee-PC>
2019-07-20 21:08:02 +0800

    经理的代码

--- src/main/java/ceo/wei/Demo.java
+++ src/main/java/ceo/wei/Demo.java
@@ -2,6 +2,6 @@ package ceo.wei;
 public class Demo {
  public static void main(String[] args) {
 	System.out.println("hello word！！");
-	System.out.println("这是我的代码！！！");
+	System.out.println("经理的代码！");
  }
 }
