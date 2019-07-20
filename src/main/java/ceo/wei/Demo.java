b534d620cd91d963443f773b75a3bd1d319453a8
Administrator <Administrator@Hasee-PC>
2019-07-20 20:49:55 +0800

    经理提交

--- src/main/java/ceo/wei/Demo.java
+++ src/main/java/ceo/wei/Demo.java
@@ -2,6 +2,6 @@ package ceo.wei;
 public class Demo {
  public static void main(String[] args) {
 	System.out.println("hello word！！");
-	System.out.println("这是我的分支，不是公司的主分支！！！ ");
+	System.out.println("经理的代码！！");
  }
 }
