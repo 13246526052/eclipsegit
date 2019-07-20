dd0f442a6157387f7175d922681c2a7f460942da
Administrator <Administrator@Hasee-PC>
2019-07-20 21:10:51 +0800

    我提交代码

--- src/main/java/ceo/wei/Demo.java
+++ src/main/java/ceo/wei/Demo.java
@@ -2,6 +2,8 @@ package ceo.wei;
 public class Demo {
  public static void main(String[] args) {
 	System.out.println("hello word！！");
+ }
+ public static void mains(String[] args) {
 	System.out.println("这是我的代码！！！");
  }
 }
