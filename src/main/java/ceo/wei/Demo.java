37f9929b6e76b30ddd29a3e90efb00bfc5f2605a
Administrator <Administrator@Hasee-PC>
2019-07-20 20:12:19 +0800

    开发着1

--- src/main/java/ceo/wei/Demo.java
+++ src/main/java/ceo/wei/Demo.java
@@ -2,6 +2,6 @@ package ceo.wei;
 public class Demo {
  public static void main(String[] args) {
 	System.out.println("hello word！！");
-	System.out.println("我不好哦！！！");
+	System.out.println("2说不要这行，");
  }
 }
