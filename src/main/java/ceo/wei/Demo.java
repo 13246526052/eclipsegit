fce69dda2edd72f3420408fa9fc4639e7e517a96
Administrator <Administrator@Hasee-PC>
2019-07-20 20:03:21 +0800

    Eclipse git 2提交修改！！

--- src/main/java/ceo/wei/Demo.java
+++ src/main/java/ceo/wei/Demo.java
@@ -2,6 +2,6 @@ package ceo.wei;
 public class Demo {
  public static void main(String[] args) {
 	System.out.println("hello word！！");
-	System.out.println("你好啊！！");
-}
+	System.out.println("我不好哦！！！");
+ }
 }
